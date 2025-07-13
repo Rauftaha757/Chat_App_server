98d154a20182e10fff1f934d9cc4541505195f9e
Taha Rauf <rauftaha757@gmail.com>
2025-07-13 14:40:17 +0500

    new file

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-
+const db_name = require('../../constants');
+require("dotenv").config()
 const DBConnect = async () => {
-    const dburl = process.env.DBURL;
-    if (!dburl) {
-        console.log("MongoDB URL not found in environment variables.");
-        return;
-    }
-
+    const dburl =`${ process.env.DBURL}/${db_name}`;
+if (!dburl) {
+    console.log("MongoDB URL not found in environment variables.");
+    return;
+}
     try {
         const connectionInstance = await mongoose.connect(dburl);
         console.log(`MongoDb connected: ${connectionInstance.connection.host}`);
