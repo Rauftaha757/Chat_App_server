1b0363917657766e70810e49b678351bdb13b643
Taha Rauf <rauftaha757@gmail.com>
2025-07-13 14:41:05 +0500

    new file

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+// To DO
+// user schema
+// generate tokens
+// pre save hook 
+//compare method
+// avatar upload cloudinary
+const { default: mongoose } = require("mongoose");
+
+const UserSchema =new mongoose.Schema({
+
+})
