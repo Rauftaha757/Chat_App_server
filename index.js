e52e1ac3d1b34ad92b79ab00e880beecc732fa8d
Taha Rauf <rauftaha757@gmail.com>
2025-07-13 14:39:55 +0500

    new file

---
+++ index.js
@@ -0,0 +1,17 @@
+const { connect } = require("mongoose");
+const DBConnect = require("./src/config/database");
+const app = require("./app");
+require("dotenv").config();
+const http = require("http");
+const setupSocket = require("./src/sockets/sockets");
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+// Setup socket.io
+const io = setupSocket(server);
+
+DBConnect().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+});
