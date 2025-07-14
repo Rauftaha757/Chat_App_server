3b4ceddc99d5e301cedf2415332135df8a5a8f0e
Taha Rauf <rauftaha757@gmail.com>
2025-07-14 13:52:36 +0500

    modified

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,11 +1,85 @@
 // To DO
-// user schema
-// generate tokens
-// pre save hook 
-//compare method
-// avatar upload cloudinary
-const { default: mongoose } = require("mongoose");
+// user schema                   done
+// pre save hook                 done
+// generate tokens               done
+//compare method                 done
+// avatar uploadcloudinary       done
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
-const UserSchema =new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true,
+    lowercase:true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: "Your Email is not formatted correctly.",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: function (pass) {
+        return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(pass);
+      },
+      message:
+        "Password must be at least 8 characters long and include at least one letter and one number.",
+    },
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  refershtoken: {
+    type: String,
+    default:""
+  },
+});
 
-})
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+const UserModel = mongoose.model("Users", UserSchema);
+module.exports = UserModel;
