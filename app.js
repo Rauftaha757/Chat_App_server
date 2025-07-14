2270d02645ff166f2d9009109ac7fc831105b34e
Taha Rauf <rauftaha757@gmail.com>
2025-07-14 13:52:03 +0500

    modified

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("./public/temp"))
 // routes
-
+const authrouter=require('./routes/user.routes.js')
+app.use("/api",authrouter)
 module.exports = app;
